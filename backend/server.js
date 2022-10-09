95b9e1ecd2fcdb05565a531cc2c8801b03d8d5b2
yanfer <yanfer.daniel@gmail.com>
2022-10-08 22:06:23 -0500

    created product screen + helmet + stock logic

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,15 @@ const app = express();
 app.use(cors());
 
 app.get('/api/products', async (req, res) => {
-  try {
-    await res.send(data.products);
-    console.log('data =', data.products);
-  } catch (error) {
-    console.log(error);
+  res.send(data.products);
+});
+
+app.get('/api/products/slug/:slug', async (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
   }
 });
 
