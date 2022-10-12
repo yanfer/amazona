cd5065bedc0bc2fe4df233ef2986e04d19237c10
yanfer <yanfer.daniel@gmail.com>
2022-10-11 20:05:47 -0500

    productServer page and MongoDB moved data

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import data from './data.js';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -16,28 +18,11 @@ mongoose
   });
 
 const app = express();
-
-app.get('/api/products', async (req, res) => {
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+/* app.get('/api/products', async (req, res) => {
   res.send(data.products);
-});
-
-app.get('/api/products/slug/:slug', async (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
-
-app.get('/api/products/:id', async (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
+}); */
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
